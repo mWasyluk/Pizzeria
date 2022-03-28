885b0e5f31cdda72f4d6acf80c00aa249c6b7b1b
MVasio <marekwasyluk@wp.pl>
2022-03-28 22:09:12 +0200

    Add basic HTML file, controller and test class for home page.

---
+++ src/main/java/pl/mvasio/pizzeria/HomePageController.java
@@ -0,0 +1,13 @@
+package pl.mvasio.pizzeria;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home.html";
+    }
+}
---
+++ src/test/java/pl/mvasio/pizzeria/HomePageControllerTest.java
@@ -0,0 +1,25 @@
+package pl.mvasio.pizzeria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomePageController.class)
+public class HomePageControllerTest {
+
+    @Autowired
+    private MockMvc mock;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mock.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home.html"));
+    }
+}
