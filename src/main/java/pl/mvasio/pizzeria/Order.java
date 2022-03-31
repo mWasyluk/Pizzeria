515a67e786b22969e376915a88f2cac7cf42770f
MVasio <marekwasyluk@wp.pl>
2022-03-31 13:54:30 +0200

    Added loading variable function to form.

--- src/main/java/pl/mvasio/pizzeria/Order.java
+++ src/main/java/pl/mvasio/pizzeria/Order.java
@@ -8,7 +8,7 @@ public class Order {
     private String street;
     private String city;
     private String zipCode;
-    private String ccNo;
+    private String ccNumber;
     private String ccExpiration;
     private String ccCVV;
 }
