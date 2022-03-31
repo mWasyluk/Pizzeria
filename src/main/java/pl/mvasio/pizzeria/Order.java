0eeacd3d1b3b978f6ba36fd7df6e5ce5af4342a6
MVasio <marekwasyluk@wp.pl>
2022-03-31 19:34:50 +0200

    Implemented validation for order form data.

--- src/main/java/pl/mvasio/pizzeria/Order.java
+++ src/main/java/pl/mvasio/pizzeria/Order.java
@@ -1,14 +1,26 @@
 package pl.mvasio.pizzeria;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 
 @Data
 public class Order {
+
+    @NotBlank (message = "To pole jest wymagane.")
     private String name;
+    @NotBlank (message = "To pole jest wymagane.")
     private String street;
+    @NotBlank (message = "To pole jest wymagane.")
     private String city;
+    @Pattern(regexp = "^(([0-9]{2})-([0-9]{3}))$", message = "Niepoprawny kod pocztowy. Wymagany format to 00-000.")
     private String zipCode;
+    @CreditCardNumber (message = "Niepoprawny numer karty kredytowej.")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])(\\/)([2-9][0-9])$", message = "Niepoprawna data ekspiracji. Wymagany format to MM/RR.")
     private String ccExpiration;
+    @Pattern(regexp = "^([0-9]{3})$", message = "Niepoprawny numer CVV. Numer powinien składać się z trzech cyfr.")
     private String ccCVV;
 }
--- src/main/java/pl/mvasio/pizzeria/OrderController.java
+++ src/main/java/pl/mvasio/pizzeria/OrderController.java
@@ -3,9 +3,12 @@ package pl.mvasio.pizzeria;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -19,9 +22,15 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder ( Order order ){
+    public String processOrder (@Valid Order order , Errors errors){
+        if (errors.hasErrors()) {
+            for (ObjectError e : errors.getAllErrors())
+                log.info(e.toString());
+            return "orderForm.html";
+        }
+
         log.info("Supplied order: " + order);
-        return "redirect:/";
+        return "";
     }
 
 }
