3d51ee9e7a9ea1199117c45720c929ab65c89388
MVasio <marekwasyluk@wp.pl>
2022-05-15 13:30:08 +0200

    feat: add pagination to the recent user's orders

--- src/main/java/pl/mvasio/pizzeria/controller/OrderController.java
+++ src/main/java/pl/mvasio/pizzeria/controller/OrderController.java
@@ -1,7 +1,11 @@
 package pl.mvasio.pizzeria.controller;
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +14,8 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import pl.mvasio.pizzeria.data.IngredientRepository;
-import pl.mvasio.pizzeria.model.Order;
 import pl.mvasio.pizzeria.data.OrderRepository;
+import pl.mvasio.pizzeria.model.Order;
 import pl.mvasio.pizzeria.model.User;
 
 import javax.validation.Valid;
@@ -22,10 +26,13 @@ import java.util.List;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties (prefix = "gmpizza.orders")
 public class OrderController {
 
     private final OrderRepository orderRepo;
     private final IngredientRepository ingredientRepo;
+    @Setter
+    private int pageSize = 5;
 
     @Autowired
     public OrderController (OrderRepository orderRepo, IngredientRepository ingredientRepo){
@@ -65,11 +72,6 @@ public class OrderController {
         return ingredientRepo;
     }
 
-    @ModelAttribute (name = "userOrders")
-    public List<Order> getUserOrders(@ModelAttribute("currentUsername") String username){
-        return orderRepo.findAllByUsernameOrderByCreateDateDesc(username);
-    }
-
     @GetMapping("/current")
     public String getCurrentOrderDetails(Model model){
         if (model.getAttribute("order") == null)
@@ -83,9 +85,21 @@ public class OrderController {
         return "currentOrder.html";
     }
 
-    @GetMapping
-    public String getUserOrders(){
+    @GetMapping()
+    public String getUserOrders(@ModelAttribute("currentUsername")  String username,
+                                @RequestParam(required = false, name = "page", defaultValue = "1") long page,
+                                Model model){
+        long ordersQuantity = orderRepo.countByUsername(username);
+        long maxPageNumber = ordersQuantity % pageSize == 0 ? ordersQuantity / pageSize : ordersQuantity / pageSize + 1;
+        model.addAttribute("maxPageNumber", maxPageNumber);
+
+        long fixedPage = page > maxPageNumber ? maxPageNumber : page;
+        Pageable pageable = PageRequest.of((int) (fixedPage - 1), pageSize);
+        model.addAttribute("pageNumber", fixedPage);
+
+        List<Order> orders = orderRepo.findAllByUsernameOrderByCreateDateDesc(username, pageable);
+        model.addAttribute("pagedOrders", orders);
+
         return "userOrders.html";
     }
-
 }
--- src/main/java/pl/mvasio/pizzeria/data/OrderRepository.java
+++ src/main/java/pl/mvasio/pizzeria/data/OrderRepository.java
@@ -1,5 +1,6 @@
 package pl.mvasio.pizzeria.data;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import pl.mvasio.pizzeria.model.Order;
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends MongoRepository<Order, String> {
     List<Order> findAllByUsernameOrderByCreateDateDesc(String username);
+    List<Order> findAllByUsernameOrderByCreateDateDesc(String username, Pageable pageable);
+    long countByUsername(String username);
 }
